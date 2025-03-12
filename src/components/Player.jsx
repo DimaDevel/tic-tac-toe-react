646dcdf70ddae47c5992c84cc9b1a27332f12293
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-03-12 15:02:36 +0100

    Updated Game Board functionality.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   function handleEditClick() {
@@ -10,7 +10,7 @@ export default function Player({ initialName, symbol }) {
     setPlayerName(event.target.value);n
   }
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {!isEditing && <span className="player-name">{playerName}</span>}
         {isEditing && <input type="text" required value={playerName} onChange={handelChange}></input>}

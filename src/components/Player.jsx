2aab43d8e9f5e778095a1a5984d8393661156b04
Dima Ivanov <dima.ivanov.dev@gmail.com>
2025-03-12 11:20:01 +0100

    Added Player component.

---
+++ src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(isEditing => !isEditing );
+  }
+  function handelChange(event) {
+    setPlayerName(event.target.value);
+  }
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && <input type="text" required value={playerName} onChange={handelChange}></input>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
